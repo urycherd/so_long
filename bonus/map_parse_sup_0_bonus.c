16515c9ed65060f5d28e0ecf62e8196e3433a75e
urycherd <89844627+urycherd@users.noreply.github.com>
2022-06-08 16:50:55 +0300

    Update map_parse_sup_0_bonus.c

--- bonus/map_parse_sup_0_bonus.c
+++ bonus/map_parse_sup_0_bonus.c
@@ -47,10 +47,10 @@ static int	length_mistake(char *line, int lenght)
 	}
 	while (i != lenght)
 	{
-		if (ft_strchr("01CEP", line[i++]) != 0)
-			return (1);
+		if (ft_strchr("01CEP", line[i++]) == NULL)
+			return (0);
 	}
-	return (0);
+	return (1);
 }
 
 static void	flag_inicilization(t_flags *wow)
